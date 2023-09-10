2a130f3150d4030dbb30ee8ffda283295246e523
Erice Dela Cruz <104292436+xtra-rice@users.noreply.github.com>
2023-09-11 03:37:49 +0800

    Update RockPaperScissor.py
    fix bug

--- Rock Paper Scissor/RockPaperScissor.py
+++ Rock Paper Scissor/RockPaperScissor.py
@@ -1,53 +1,36 @@
 import random
 
+options = (["r", "p", "s"])
+user_score = 0
+computer_score = 0
+print("Welcome to ROCK, PAPER, SCISSOR!")
 
-# def play():
-#     print("What's your pick? 'Rock', 'Paper', 'Scissor'")
-#     user = input("'r' for rock, 'p' for paper, 's' for scissor: ")
-#     computer = random.choice(['r', 'p', 's'])
-
-#     if user == computer:
-#         return "It's a tie"
-#     # r > s, p > r, s > p
-
-#     if is_Win(user, computer):
-#         return "You won!"
-#     return "You lost"
-
-# def is_Win(player, opponent):
-#     if (player == 'r' and opponent == 's') or (player == 'p' and opponent == 'r') or (player == 's' and opponent == 'p'):
-#         return True
-
-
-# print(play())
- 
 while True:
-    # User's choice
-    user_choice = input(
-        "Enter your choice (Rock, Paper, Scissors) or 'q' to quit: ").lower()
+    user = input("Choose your pick 'r', 'p', 's' or 'q' to quit. ").lower()
+    if user == 'q':
+        break
+    print()
 
-    if user_choice == 'q':
-        break  # Exit the game if the user enters 'q'
-
-    # Computer's choice
-    choices = ['r', 'p', 's']
-    computer_choice = random.choice(choices)
-
-    # Determine the winner
-    if user_choice in choices:
-        print(f"Computer chose {computer_choice}")
-        if user_choice == computer_choice:
-            print("It's a tie!")
-        elif (
-            (user_choice == 'rock' and computer_choice == 'scissors') or
-            (user_choice == 'paper' and computer_choice == 'rock') or
-            (user_choice == 'scissors' and computer_choice == 'paper')
-        ):
-            print("You win!")
-        else:
-            print("Computer wins!")
+    if user not in options:
+        print("Invalid Input. Try again.")
     else:
-        print("Invalid choice. Please choose Rock, Paper, or Scissors.")
+        computer = random.choice(options)
 
-# Game over
+        if computer == user:
+            print("It's a tie")
+        elif (user == 'r' and computer == 's') or (user == 'p' and computer == 'r') or (user == 's' and computer == 'p'):
+            print("You win!")
+            user_score += 1
+        else:
+            print("Computer win!")
+            computer_score += 1
+
+    if (user_score == 5):
+        print("Congratulations! You got first 5 points")
+        break
+
+    elif (computer_score == 5):
+        print("Sorry, the computer beat you to 5 points")
+        break
+
 print("Thanks for playing!")
